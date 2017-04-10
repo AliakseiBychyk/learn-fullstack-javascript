5d6b4e41129edf799a4f44bce059be78b06f4e00
Aliaksei Bychyk <Aliaksei_Bychyk@epam.com>
2017-04-10 16:34:47 +0300

    clean index.js from comments

--- src/index.js
+++ src/index.js
@@ -3,22 +3,7 @@ import ReactDOM from 'react-dom';
 
 import App from './components/App';
 
-// App.propTypes = {
-//   headerMessage: React.PropTypes.string.isRequired
-// };
-
-// App.defaultProps = {
-//   headerMessage: 'Hello!!'
-// };
-
 ReactDOM.render(
-  //React.createElement('h2', null, 'Hello React!'),
-
-  // <h2 className="text-center" style={{color}}>
-  //   Hello React with JSX!! -- {Math.random()}
-  // </h2>,
-
-  // <App headerMessage="Hello props!" />,
   <App />,
   document.getElementById('root')
 );
