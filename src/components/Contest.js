954d66eedaa8071e684825a74347bf7803e23e9c
Aliaksei Bychyk <Aliaksei_Bychyk@epam.com>
2017-04-13 19:28:59 +0300

    writing the proposed new name form

--- src/components/Contest.js
+++ src/components/Contest.js
@@ -4,6 +4,14 @@ class Contest extends Component {
   componentDidMount() {
     this.props.fetchNames(this.props.nameIds);
   }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    // read the value that the user typed
+    this.props.addName(this.refs.newNameInput.value, this.props._id);
+    this.refs.newNameInput.value = '';
+  };
+
   render() {
     return (
       <div className="Contest">
@@ -38,15 +46,15 @@ class Contest extends Component {
             <h3 className="panel-title">Propose a New Name</h3>
           </div>
           <div className="panel-body">
-            <form>
+            <form onSubmit={this.handleSubmit}>
               <div className="input-group">
                 <input type="text"
-                   placeholder="New Name Here..."
-                   ref="newNameInput"
-                   className="form-control" />
+                  placeholder="New Name Here..."
+                  ref="newNameInput"
+                  className="form-control" />
                 <span className="input-group-btn">
                   <button type="submit" className="btn btn-info">
-                     Sumbit
+                    Sumbit
                   </button>
                 </span>
               </div>
@@ -64,11 +72,13 @@ class Contest extends Component {
 }
 
 Contest.propTypes = {
+  _id: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
   contestListClick: PropTypes.func.isRequired,
   fetchNames: PropTypes.func.isRequired,
   nameIds: PropTypes.array.isRequired,
-  lookupName: PropTypes.func.isRequired
-}
+  lookupName: PropTypes.func.isRequired,
+  addName: PropTypes.func.isRequired,
+};
 
 export default Contest;
